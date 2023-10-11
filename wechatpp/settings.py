913fa30f4f300b52e1b029107de34fdc6fb98989
mithai01234 <146544284+mithai01234@users.noreply.github.com>
2023-10-11 11:57:39 +0530

    Update settings.py

--- wechatpp/settings.py
+++ wechatpp/settings.py
@@ -160,23 +160,4 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True 
 
 
-CORS_ALLOW_METHODS = [
-    "GET",
-    "POST",
-    "PUT",
-    "PATCH",
-    "DELETE",
-    "OPTIONS",
-]
 
-CORS_ALLOW_HEADERS = [
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "dnt",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
-]
